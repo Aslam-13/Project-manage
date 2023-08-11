a45a57ae8e5911492eb3f9ebb9f2e074f102e6ca
syed aslam uddin <syedaslamuddin74@gmail.com>
2023-08-11 20:17:53 +0530

    components and pages

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/components/Authform"
+export default function Register( ){
+  return <div>
+    <AuthForm mode="register"/>
+  </div>
+}
---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,6 @@
+import AuthForm from "@/components/Authform"
+export default function Signin( ){
+  return <div>
+    <AuthForm mode="signin"/>
+  </div>
+}
